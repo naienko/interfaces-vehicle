1455495c93cb9dfc764445edb74a37765f37e944
Panya Farnette <naienko@gmail.com>
2019-04-17 09:43:49 -0500

    exercise setup

---
+++ IVehicle.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Garage
+{
+
+	public interface IVehicle
+	{
+		int Wheels { get; set; }
+		int Doors { get; set; }
+		int PassengerCapacity { get; set; }
+		bool Winged { get; set; }
+		string TransmissionType { get; set; }
+		double EngineVolume { get; set; }
+		double MaxWaterSpeed { get; set; }
+		double MaxLandSpeed { get; set; }
+		double MaxAirSpeed { get; set; }
+		void Start();
+		void Stop();
+		void Drive();
+		void Fly();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Garage
+{
+    public class Program
+{
+
+    public static void Main() {
+
+        // Build a collection of all vehicles that fly
+
+        // With a single `foreach`, have each vehicle Fly()
+
+
+
+        // Build a collection of all vehicles that operate on roads
+
+        // With a single `foreach`, have each road vehicle Drive()
+
+
+
+        // Build a collection of all vehicles that operate on water
+
+        // With a single `foreach`, have each water vehicle Drive()
+    }
+
+}
+}
